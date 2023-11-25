704d5bc27ecff2b911810eb13ae580f388f6163f
nordivi <victorw.nordi@icloud.com>
2023-11-25 18:06:11 -0300

    climbing stairs

---
+++ one_D_programming/climbing_stairs.py
@@ -0,0 +1,12 @@
+# You are climbing a staircase. It takes n steps to reach the top.
+# Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+
+def climbStairs(n: int) -> int:
+    one, two = 1, 1
+
+    for i in range(n-1):
+        temp = one
+        one = one + two
+        two = temp
+    return one
