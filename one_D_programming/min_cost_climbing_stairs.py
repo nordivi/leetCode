ea0d3cc9280085d95164e9a3d623989f151258af
nordivi <victorw.nordi@icloud.com>
2024-03-04 21:12:56 -0300

    :sparkles: min cost climbing stairs

--- one_D_programming/min_cost_climbing_stairs.py
+++ one_D_programming/min_cost_climbing_stairs.py
@@ -5,8 +5,19 @@
 # Return the minimum cost to reach the top of the floor.
 
 from typing import List
+
+
 def minCostClimbingStairs(cost: List[int]) -> int:
-    cost.append(0)
-    for i in range(len(cost)-3, -1, -1):
-        cost[i] += min(cost[i+1], cost[i+2])
-    return min(cost[0], cost[1])
+    if not cost:
+        return 0
+    l = len(cost)
+    df = [0] * l
+    df[0] = cost[0]
+    if l > 1:
+        df[1] = cost[1]
+    for i in range(2, l):
+        df[i] = cost[i] + min(df[i - 1], df[i - 2])
+    return min(df[-1], df[-2])
+
+
+minCostClimbingStairs(cost = [1,100,1,1,1,100,1,1,100,1])
